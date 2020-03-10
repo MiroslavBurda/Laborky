48ce4d6cd213d2085051bab90cf29a9b05d657f0
MiroslavBurda <burda.miroslav@gmail.com>
2020-03-10 15:52:16 +0100

    drobne zmeny

--- ALKS/src/main.cpp
+++ ALKS/src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include "ALKS.h"
 
+int p1 = 0, p2 = 0; // define integer 
+float cm = 0 ;      // define real 
 
 void setup() // this part run once
 {
     setupAll(); // // initialize all LED digital pins as an output, SW pins as input etc., for more info press F12
+    Serial.begin(115200); // initialize UART, speed 115200 bit/s
     for(uint8_t i=0; i<2; i++) // 2x blink with all LED 
     {
         delay (500); // pause 500 miliseconds
@@ -18,34 +21,21 @@ void setup() // this part run once
         digitalWrite (L_G , LOW ); 
         digitalWrite (L_B , LOW );
     }
-    // L_RGB_R
-    digitalWrite (L_RGB_R , HIGH ); // switch on red color in RGB LED
-    delay (500);
-    digitalWrite (L_RGB_R , LOW ); 
-    delay (500);
-    digitalWrite (L_RGB_G , HIGH ); // switch on green color in RGB LED
-    delay (500);
-    digitalWrite (L_RGB_G , LOW );
-    delay (500);
-    digitalWrite (L_RGB_B , HIGH ); // switch on blue color in RGB LED
-    delay (500); 
-    digitalWrite (L_RGB_B , LOW ); 
-    delay (500);
-    digitalWrite (L_RGB_R , HIGH );
-    digitalWrite (L_RGB_G , HIGH );
-    digitalWrite (L_RGB_B , HIGH );
-    delay (500);
-    digitalWrite (L_RGB_R , LOW );
-    digitalWrite (L_RGB_G , LOW );
-    digitalWrite (L_RGB_B , LOW );
-
+    
     ledcSetup (0, 1000 , 10); // create PWM
     // ledcSetup ( channel , freq , resolution )
     // channel = 0 - 15
     // resolution = 10 => 2^10 => 1024
     ledcAttachPin (L_B , 0); // ledcAttachPin (pin , channel )
-  
+    printf("%i: %i ;", 1, analogRead ( POT1 )/4); // write data for serial monitor via UART 
+    printf("%i: %i \n", 2, analogRead ( POT2 )/4); 
+ 
+    p1 = analogRead (POT1); 
+    p2 = analogRead (POT2); 
+    printf("%i, %i, %3.2f \n", p1 + p2, 2*p1*p2, ( (float) p1-p2)/7 ); // %i - integer, %f - float, \n - new line
 }
+
+
 
 void loop() // this part works in cycle
 {  
@@ -61,7 +51,11 @@ void loop() // this part works in cycle
         { digitalWrite (L_G , HIGH );}
     else { digitalWrite (L_G , LOW );}
 
-    ledcWrite (0, analogRead ( POT1 )/4); // potentiometer connect
+      printf("%i: %i ; ", 1, analogRead ( POT1 ) ); // write value of potenciometer
+      printf("%i: %i \n", 2, analogRead ( POT2 ) );
+    
+ledcWrite (0, analogRead ( POT1 )/4); // potentiometer connect to PWM   
+
 
 
 }
--- mikrokontrolery/src/main.cpp
+++ mikrokontrolery/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 // #include "blikani.h"
-// #include "tlacitka_ledky.h"
+#include "tlacitka_ledky.h"
 // #include "tlacitka_serial.h"
-#include "tlacitka_kliknuti.h"
+// #include "tlacitka_kliknuti.h"
 
 extern void loop();
 extern void setup();
