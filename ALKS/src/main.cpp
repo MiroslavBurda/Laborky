18b66a0be453fe245420cc0b05870a1ac378a962
MiroslavBurda <burda.miroslav@gmail.com>
2020-04-13 15:50:13 +0200

    fixed some errors

--- ALKS/src/main.cpp
+++ ALKS/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include "ALKS.h"
+// #include "Wire.h"
+#include <analogWrite.h>
 
 int p1 = 0, p2 = 0, a = 0; // define integer 
 float cm = 0 ;      // define real 
@@ -7,6 +9,7 @@ float cm = 0 ;      // define real
 void setup() // this part run once
 {
     setupAll(); // // initialize all LED digital pins as an output, SW pins as input etc., for more info press F12
+    pinMode(27, OUTPUT);
     Serial.begin(115200); // initialize UART, speed 115200 bit/s
     for(uint8_t i=0; i<2; i++) // 2x blink with all LED 
     {
@@ -24,12 +27,12 @@ void setup() // this part run once
     }
     
     ledcSetup (0, 1000 , 10); // create PWM
-    ledcSetup(1,1000, 10 );
+    ledcSetup(1, 1000, 10 );
     // ledcSetup ( channel , freq , resolution )
     // channel = 0 - 15
     // resolution = 10 => 2^10 => 1024
     ledcAttachPin (L_B , 0); // ledcAttachPin (pin , channel )
-    ledcAttachPin (L_R , 1);
+    ledcAttachPin (27 , 1);
     printf("%i: %i ;", 1, analogRead ( POT1 )/4); // write data for serial monitor via UART 
     printf("%i: %i \n", 2, analogRead ( POT2 )/4); 
  
@@ -42,7 +45,7 @@ void setup() // this part run once
 
 void loop() // this part works in cycle
 {  
-  /*  if (( digitalRead ( SW1 )) == LOW ) // pushed switch 1 
+    if (( digitalRead ( SW1 )) == LOW ) // pushed switch 1 
         { digitalWrite (L_R , HIGH );}
     else { digitalWrite (L_R , LOW );}
 
@@ -52,16 +55,16 @@ void loop() // this part works in cycle
 
     if (( digitalRead ( SW3 )) == LOW )
         { digitalWrite (L_G , HIGH );}          
-    else { digitalWrite (L_G , LOW );} */    
+    else { digitalWrite (L_G , LOW );}    
 
-    //  printf("%i: %i ; ", 1, analogRead ( POT1 ) );
-    // printf("%i: %i \n", 2, analogRead ( POT2 ) );
+    printf("%i: %i ; ", 1, analogRead ( POT1 ) );
+    printf("%i: %i \n", 2, analogRead ( POT2 ) );
 
-    delay (300); // pause 300 miliseconds
+ /*   delay (300); // pause 300 miliseconds
         digitalWrite (L_R , HIGH ); // switch on red LED
     delay (300); 
         digitalWrite (L_R , LOW ); // switch off red LED
-if ( ( digitalRead ( SW1 )) == LOW )
+ if ( ( digitalRead ( SW1 )) == LOW )
 {
     a=a+200;
     delay(500);
@@ -70,16 +73,19 @@ if ( ( digitalRead ( SW2 )) == LOW )
 {
     a=0;
     delay(500);
-}
-; // potentiometer connect to PWM   
+} */ 
 
-ledcWrite (1, a); // potentiometer connect to PWM   
+
+// ledcWrite (1, a); // potentiometer connect to PWM 
+
 
       printf("%i: %i ; ", 1, analogRead ( POT1 ) ); // write value of potenciometer
       printf("%i: %i \n", 2, analogRead ( POT2 ) );
     
-ledcWrite (0, analogRead ( POT1 )/4); // potentiometer connect to PWM   - ahoj 
+ledcWrite (0, analogRead ( POT2 )/4); // potentiometer connect to PWM
+ledcWrite (1, analogRead ( POT2 )/4);
+// ledcWrite (0, analogRead ( POT1 )/4);
 
-
+// analogWrite(27, POT1, 4095);
 
 }
